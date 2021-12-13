78f955ab9aed71d5190fbafc44620cac360f5829
Lyle-Rogers <lylerogers8@gmail.com>
2021-12-12 21:21:05 -0700

    I'm a fucking liturall perfesionallllllllllll.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [tracks, setTracks] = useState([])
 
-  const getTracks = () => {
-    const tracks = [];
+  useEffect(() => {
     axios.get('https://django-tester111.herokuapp.com/track/')
       .then(res => {
-        console.log('the tracks from heroku', res)
-        tracks = res;
+        setTracks(res.data)
+        console.log('the tracks from heroku', res.data)
       })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
 
-    console.log('tracks', tracks);
+  const displayTracks = () => {
+    console.log('The tracks', tracks)
 
-    return tracks.map(track => {
+    return tracks.map((track) => {
       return (
-        <div>
+        <div className="track">
           <h1>{track.title}</h1>
           <h1>{track.description}</h1>
         </div>
       )
+    })
+  }
+
+  const aNewTrack = () => {
+    axios.post('https://django-tester111.herokuapp.com/track/', {
+      title: 'we created the track',
+      description: 'Life has been given to this new track we created.'
+    })
+    .catch((err) => {
+      console.error(err);
     })
   }
 
@@ -39,7 +55,15 @@ function App() {
         >
           Learn React
         </a>
-        {getTracks()}
+        <button 
+          className="thebtnIknow!" 
+          onClick={() => aNewTrack()}
+        >
+          a new track.!.!.!.
+        </button>
+        <div className="tracks">
+          {displayTracks()}
+        </div>
       </header>
     </div>
   );
